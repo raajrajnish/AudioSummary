d13718092ca19daf209e495396747b7267076b36
rajnish <raajrajnish@gmail.com>
2019-11-06 19:28:33 +0530

    first commit

--- mainfolder/service_start.py
+++ mainfolder/service_start.py
@@ -2,6 +2,8 @@ from extract_summary import summary as s
 from speech_to_text import transcribe, blob_upload
 from flask import Flask, request, render_template
 from datetime import datetime
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -14,12 +16,33 @@ def index():
 @app.route('/start_analysis', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-
         f = request.files['file']
-        print("File Name is : ", f.filename)
-        blob_upload(f.filename, f.read())
-        transcribe(f.filename)
-        summary = s("text_file/extracted_text_from_audio.txt")
-        return render_template("success.html", summary=summary,starttime=current_time)
+        name = str(f)
+        if '.mp3' in name:
+            print("Starting the audio to text to summary process.")
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+
+            f = request.files['file']
+            print("File Name is : ", f.filename)
+            blob_upload(f.filename, f.read())
+            transcribe(f.filename)
+            summary = s("text_file/extracted_text_from_audio.txt")
+            return render_template("success.html", summary=summary, starttime=current_time)
+
+        elif '.txt' in name:
+            os.makedirs(os.path.join(app.instance_path, 'uploaded_file'), exist_ok=True)
+            filepath = 'text_file/'
+            print("Starting text to summary process.")
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            f = request.files['file']
+            f.save(os.path.join(app.instance_path, 'uploaded_file', secure_filename(f.filename)))
+            summary = s('instance/uploaded_file/'+f.filename)
+            return render_template("success.html", summary=summary, starttime=current_time)
+
+        else :
+            return render_template('home.html')
+
+
+
